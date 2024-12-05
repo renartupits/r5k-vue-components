422e057ad1c9e09732fa2693f07d84342aeec5bb
Renar Tupits <renar.tupits@kwota.com>
2024-12-05 11:46:39 +0200

    init project

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import { resolve } from "path";
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, "src/main.ts"),
+      name: "R5kVueComponents",
+      // the proper extensions will be added
+      fileName: "r5k-vue-components",
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["vue"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+});
